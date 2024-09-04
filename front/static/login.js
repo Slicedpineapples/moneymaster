c05f690b7984020d9ed53854b9f077c07cc18d52
pc52 <nyabutofelix@outlook.com>
2024-09-04 20:48:52 +0200

    Error affecting api endpoint link cleared

--- front/static/login.js
+++ front/static/login.js
@@ -3,24 +3,15 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
     //Stripping empty spaces from the username
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value;
-
-    // Determine the base URL based on the hostname
-// Get the hostname and IP address
     const hostname = window.location.hostname;
-
-    // Define your local network IP address (e.g., 192.168.x.x)
-    const localNetworkIp = '192.168.x.x';
-
-    // Set the API URL based on the hostname
-    if (hostname === 'localhost' || hostname === '127.0.0.1' || hostname === localNetworkIp) {
-        // Use the localhost or local network IP for development
-        apiUrl = `http://${localNetworkIp}:5000/apiLogin`;
+    let apiUrl;
+    if (hostname === 'localhost' || hostname === '127.0.0.1') {
+        apiUrl = 'http://127.0.0.1:5000/';
     } else {
-        // Use the server's IP address or hostname for production
-        apiUrl = `http://${hostname}:5000/apiLogin`;
-        console.log(apiUrl);
+        apiUrl = `http://${hostname}:5000/`;
     }
-
+    // console.log(apiUrl) //Debugging 
+    
     const response = await fetch(apiUrl, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
